39e1dfd7e193bdfa4aece938236701d83aacb0a8
Krzysztof Kuik <krzysztof.kuik@hotmail.com>
2019-06-08 14:16:33 +0200

    header section update

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,23 @@
 $(()=>{
     // console.log('App Loaded!!!');
 
-    let openMenu = $('.nav-icon');
+    let openMenu = $('.nav-icon-open');
     let closeMenu = $('.nav-icon-close');
     let navBar = $('.nav-bar')
     console.log(openMenu, closeMenu, navBar);
     
+    openMenu.on('click', function(){
+        console.log('test');
+        $(this).css('visibility', 'hidden');
+        closeMenu.css('visibility', 'visible');
+        navBar.slideDown();
+    })
+
+    closeMenu.on('click', function () {
+        $(this).css('visibility', 'hidden');
+        openMenu.css('visibility', 'visible');
+        navBar.slideUp();
+    })
 
 })
 
